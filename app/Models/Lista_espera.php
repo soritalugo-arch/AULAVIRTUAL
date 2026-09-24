a8beddd526a83f89f646b0ba17929546f9b8f5a5
Beto06 <gilbeto.sifontes06@gmail.com>
2026-09-24 04:26:28 -0400

    Creacion de Modelos Parte 2

---
+++ app/Models/Lista_espera.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lista_espera extends Model
+{
+    //
+}
---
+++ app/Models/Rol_usuario.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rol_usuario extends Model
+{
+    //
+}
